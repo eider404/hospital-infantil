26c25bd4a95b3cb4e1dfd68766f9d7f0e41210d6
Eider Pool <eider.pool.arjona@gmail.com>
2023-04-15 00:32:56 -0600

    ejs eliminado

--- src/controllers/getAllPatients.js
+++ src/controllers/getAllPatients.js
@@ -3,8 +3,7 @@ const patienteModel = require('../models/patienteModel')
 const getAllPatients = async(req, res) => {
     
     const rows = await patienteModel.getAll();
-    //res.status(200).json({status:200, data: rows});
-    res.render('index', { datos: rows });
+    res.status(200).json({status:200, data: rows});
 }
 
 
